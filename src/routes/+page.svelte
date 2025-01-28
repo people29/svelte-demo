39a165fd1e2e47d259d840ecf7d85e33a2881981
Mシ <m@odds.team>
2025-01-28 10:02:27 +0700

    add layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
-	import type { PageData } from './$types';
-	export let data: PageData;
 </script>
 
 
 <h1>Welcome to SvelteKit</h1>
 <h2>Hello World</h2>
-<pre>{JSON.stringify(data.users, null, 2)}</pre>
